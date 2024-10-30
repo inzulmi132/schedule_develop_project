1c50c3c5cf8d19a3896465d5f4c70e3dab351b92
inzulmi <inzulmi132@gmail.com>
2024-10-30 16:26:24 +0900

    Style: 어노테이션 수정, 주석 수정

--- src/main/java/com/sparta/scheduledevelop/exception/CustomErrorCode.java
+++ src/main/java/com/sparta/scheduledevelop/exception/CustomErrorCode.java
@@ -1,11 +1,11 @@
 package com.sparta.scheduledevelop.exception;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 @Getter
-@AllArgsConstructor
+@RequiredArgsConstructor
 public enum CustomErrorCode {
     // user error
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저를 찾을 수 없습니다."),
@@ -26,7 +26,7 @@ public enum CustomErrorCode {
     TOKEN_UNSUPPORTED(HttpStatus.UNAUTHORIZED, "지원되지 않는 JWT 토큰입니다."),
     TOKEN_INVALID(HttpStatus.BAD_REQUEST, "잘못된 JWT 토큰입니다."),
 
-    // other error
+    // auth error
     ADMIN_PASSWORD_INCORRECT(HttpStatus.UNAUTHORIZED, "관리자 암호가 틀렸습니다."),
     INVALID_ADD_AUTHOR(HttpStatus.UNAUTHORIZED, "담당 유저를 배치할 권한이 없습니다."),
     INVALID_UPDATE(HttpStatus.UNAUTHORIZED, "수정할 권한이 없습니다."),
--- src/main/java/com/sparta/scheduledevelop/exception/CustomException.java
+++ src/main/java/com/sparta/scheduledevelop/exception/CustomException.java
@@ -1,12 +1,10 @@
 package com.sparta.scheduledevelop.exception;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public class CustomException extends RuntimeException {
     private final CustomErrorCode customErrorCode;
-
-    public CustomException(CustomErrorCode customErrorCode) {
-        this.customErrorCode = customErrorCode;
-    }
 }
