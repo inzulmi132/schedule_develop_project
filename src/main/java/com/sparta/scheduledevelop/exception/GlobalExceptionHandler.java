34f4e6b10e374d5e38b4a04762a5d77d6a4e98f6
inzulmi <inzulmi132@gmail.com>
2024-10-30 16:23:27 +0900

    Feat: Json parse error exception handler 추가

--- src/main/java/com/sparta/scheduledevelop/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/scheduledevelop/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package com.sparta.scheduledevelop.exception;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,6 +20,7 @@ public class GlobalExceptionHandler {
                 .body(errorCode.getMessage());
     }
 
+    // validation error
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
         List<String> errors = ex.getBindingResult()
@@ -29,5 +31,13 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST)
                 .body("Validation failed: " + String.join(", ", errors));
+    }
+
+    // json parse error
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ex.getMessage());
     }
 }
